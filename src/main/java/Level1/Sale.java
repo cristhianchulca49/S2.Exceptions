f060b582f02c8eef3a8ccca0e07b08f943168f48
crist <cristhianchulca49@gmail.com>
2025-11-13 13:42:22 +0100

    feat: Add Sale class with products and totalPrice attributes

---
+++ src/main/java/Level1/Sale.java
@@ -0,0 +1,10 @@
+package Level1;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sale {
+    private List<Product> products = new ArrayList<>();
+    private BigDecimal totalPrice;
+}
