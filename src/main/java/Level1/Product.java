0c624d6ae2c33a8bba2b842ff9d3846ef1f89eed
crist <cristhianchulca49@gmail.com>
2025-11-13 14:06:01 +0100

    feat: Add constructor with parameters and getters in Product class

--- src/main/java/Level1/Product.java
+++ src/main/java/Level1/Product.java
@@ -3,4 +3,17 @@ package Level1;
 public class Product {
     private String name;
     private float price;
+
+    public Product(String name, float price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
 }
