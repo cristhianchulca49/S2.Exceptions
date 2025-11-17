3aea200094582cfdc0bb1698dc7ef543a40254fe
crist <cristhianchulca49@gmail.com>
2025-11-17 12:19:58 +0100

    test: add new Product and call calculateTotalPrice()

--- src/main/java/Level1/Main.java
+++ src/main/java/Level1/Main.java
@@ -7,5 +7,12 @@ public class Main {
     static void main(String[] args) {
         Sale sale = new Sale();
         sale.calculateTotalPrice();
+
+        sale.addNewProduct(new Product("Laptop", 100));
+        sale.addNewProduct(new Product("Tablet", 200));
+        sale.addNewProduct(new Product("Mouse", 300));
+        sale.addNewProduct(new Product("Mouse", 400));
+        sale.calculateTotalPrice();
+
     }
 }
