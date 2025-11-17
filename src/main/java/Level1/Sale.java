efa235dc4ab41ebfae3768d646cd335b44766bde
crist <cristhianchulca49@gmail.com>
2025-11-17 10:24:44 +0100

    feat: Add calculateTotalPrice() method

--- src/main/java/Level1/Sale.java
+++ src/main/java/Level1/Sale.java
@@ -7,4 +7,8 @@ import java.util.List;
 public class Sale {
     private List<Product> products = new ArrayList<>();
     private BigDecimal totalPrice;
+
+    public void calculateTotalPrice() {
+
+    }
 }
