2b1ffa99caf96b30e937616f6202267f17b792d3
crist <cristhianchulca49@gmail.com>
2025-11-17 12:53:42 +0100

    feat: add robust readByte method with input validation and error handling

--- src/main/java/Level2/Input.java
+++ src/main/java/Level2/Input.java
@@ -1,23 +1,33 @@
 package Level2;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Input {
     private static Scanner sc = new Scanner(System.in);
 
-    public static byte readByte(String mensaje) {
-        return sc.nextByte();
+    public static byte readByte(String message) {
+        while (true) {
+            try {
+                System.out.println(message);
+                return sc.nextByte();
+            } catch (InputMismatchException e) {
+                System.out.println("Sorry, Format error try again");
+            } finally {
+                sc.nextLine();
+            }
+        }
     }
 
-    public static int readInt(String mensaje) {
+    public static int readInt(String message) {
         return sc.nextInt();
     }
 
-    public static float readFloat(String mensaje) {
+    public static float readFloat(String message) {
         return sc.nextFloat();
     }
 
-    public static double readDouble(String mensaje) {
+    public static double readDouble(String message) {
         return sc.nextDouble();
     }
 }
