faf2d1492319705744d2b9267f653254909df5bb
crist <cristhianchulca49@gmail.com>
2025-11-17 13:41:08 +0100

    test: add example usage of Input methods for all data types

--- src/main/java/Level2/Main.java
+++ src/main/java/Level2/Main.java
@@ -2,6 +2,25 @@ package Level2;
 
 public class Main {
     static void main(String[] args) {
+        byte age = Input.readByte("Enter your age: ");
+        System.out.println("Your age is: " + age);
 
+        int phoneNumber = Input.readInt("What's your phone number?");
+        System.out.println("Your phone number is: " + phoneNumber);
+
+        float average = Input.readFloat("Enter your school average");
+        System.out.println("Your school average is: " + average);
+
+        double  pi = Input.readDouble("What's PI's value?");
+        System.out.println("The PI value is: " + pi);
+
+        char letter = Input.readChar("Choose a Letter");
+        System.out.println("The letter is: " + letter);
+
+        String name = Input.readString("Write your name");
+        System.out.println("Your  name is: " + name);
+
+        boolean s_n = Input.readBoolean("Choose S or N");
+        System.out.println("You chose " + s_n);
     }
 }
