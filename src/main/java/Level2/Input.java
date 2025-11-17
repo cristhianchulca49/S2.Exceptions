17b8c1124c6b79be01434088398c5c3cedce2003
crist <cristhianchulca49@gmail.com>
2025-11-17 12:42:08 +0100

    feat: add input reader methods for byte, int, float and double using Scanner

--- src/main/java/Level2/Input.java
+++ src/main/java/Level2/Input.java
@@ -4,4 +4,20 @@ import java.util.Scanner;
 
 public class Input {
     private static Scanner sc = new Scanner(System.in);
+
+    public static byte readByte(String mensaje) {
+        return sc.nextByte();
+    }
+
+    public static int readInt(String mensaje) {
+        return sc.nextInt();
+    }
+
+    public static float readFloat(String mensaje) {
+        return sc.nextFloat();
+    }
+
+    public static double readDouble(String mensaje) {
+        return sc.nextDouble();
+    }
 }
