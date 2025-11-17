9ecb1d4e1ca878feb35e75f3247c91bb2e908089
crist <cristhianchulca49@gmail.com>
2025-11-17 12:18:53 +0100

    test: add new Sale object and call calculateTotalPrice() to test EmptySaleException

--- src/main/java/Level1/Main.java
+++ src/main/java/Level1/Main.java
@@ -1,7 +1,11 @@
 package Level1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     static void main(String[] args) {
-
+        Sale sale = new Sale();
+        sale.calculateTotalPrice();
     }
 }
