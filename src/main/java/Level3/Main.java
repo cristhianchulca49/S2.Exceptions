08c130503f0dbb2b0e78df7996ee61fb3f59c096
cristhianchulca49 <cristhianchulca49@gmail.com>
2026-01-08 18:23:23 +0100

    feat: create dependencies and attributes necessaries

--- src/main/java/Level3/Main.java
+++ src/main/java/Level3/Main.java
@@ -1,9 +1,16 @@
 package Level3;
 
+import Level2.Input;
+import Level3.ConsoleUI.MenuUI;
+import Level3.ReservationServices.ReservationServices;
+
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-
+        int numberRows = Input.readInt("Please, enter the number of rows: ");
+        int numberSeats = Input.readInt("Please, enter the number of seats: ");
+        ReservationServices reservationServices = new ReservationServices(numberRows, numberSeats);
+        MenuUI consoleUI = new MenuUI(reservationServices);
     }
 }
