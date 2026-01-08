170469d8c50470f1753d8300327fd393c4b16dde
cristhianchulca49 <cristhianchulca49@gmail.com>
2026-01-08 19:50:40 +0100

    feat: add validateSeatPosition() throws InvalidSeatException()

--- src/main/java/Level3/ReservationServices/ReservationServices.java
+++ src/main/java/Level3/ReservationServices/ReservationServices.java
@@ -1,4 +1,28 @@
 package Level3.ReservationServices;
 
+import Level3.Models.Seat;
+import Level3.ReservationServices.CustomExceptions.InvalidSeatException;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ReservationServices {
+    private int totalRows;
+    private int seatsPerRows;
+    private List<Seat> seats = new ArrayList<>();
+
+    public ReservationServices(int numberRows, int numberSeats) {
+        this.totalRows = numberRows;
+        this.seatsPerRows = numberSeats;
+    }
+
+    private void validateSeatPosition(int row, int seat) throws InvalidSeatException {
+        if (row < 1 || row > totalRows || seat < 1 || seat > seatsPerRows) {
+            throw new InvalidSeatException();
+        }
+    }
+
+    public void reserveSeat(int row, int numberSeat, String namePerson) {
+
+    }
 }
