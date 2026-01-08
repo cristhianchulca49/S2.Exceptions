4e1d0f6189ee6772457ac75afaab9f8e22b9d331
cristhianchulca49 <cristhianchulca49@gmail.com>
2026-01-08 20:34:01 +0100

    refactor: rename variable

--- src/main/java/Level3/Main.java
+++ src/main/java/Level3/Main.java
@@ -7,8 +7,8 @@ import Level3.ReservationServices.ReservationServices;
 public class Main {
     public static void main(String[] args) {
         int numberRows = Input.readInt("Please, enter the number of rows: ");
-        int numberSeats = Input.readInt("Please, enter the number of seats: ");
-        ReservationServices reservationServices = new ReservationServices(numberRows, numberSeats);
+        int seatNumber = Input.readInt("Please, enter the number of seats: ");
+        ReservationServices reservationServices = new ReservationServices(numberRows, seatNumber);
         MenuUI consoleUI = new MenuUI(reservationServices);
         consoleUI.showMenu();
     }
