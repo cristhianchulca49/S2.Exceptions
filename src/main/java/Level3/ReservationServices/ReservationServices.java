514112d6ca5ef31a3a6956ba9f996798969b585b
cristhianchulca49 <cristhianchulca49@gmail.com>
2026-01-08 20:38:45 +0100

    feat: add reserveSeat() throws SeatAlreadyTakenException and rename seatName variable

--- src/main/java/Level3/ReservationServices/ReservationServices.java
+++ src/main/java/Level3/ReservationServices/ReservationServices.java
@@ -2,27 +2,34 @@ package Level3.ReservationServices;
 
 import Level3.Models.Seat;
 import Level3.ReservationServices.CustomExceptions.InvalidSeatException;
+import Level3.ReservationServices.CustomExceptions.SeatAlreadyEmptyException;
+import Level3.ReservationServices.CustomExceptions.SeatAlreadyTakenException;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ReservationServices {
-    private int totalRows;
-    private int seatsPerRows;
-    private List<Seat> seats = new ArrayList<>();
+    private final int totalRows;
+    private final int seatsPerRows;
+    private final List<Seat> seatReserved = new ArrayList<>();
 
-    public ReservationServices(int numberRows, int numberSeats) {
+    public ReservationServices(int numberRows, int seatsPerRows) {
         this.totalRows = numberRows;
-        this.seatsPerRows = numberSeats;
+        this.seatsPerRows = seatsPerRows;
     }
 
-    private void validateSeatPosition(int row, int seat) throws InvalidSeatException {
-        if (row < 1 || row > totalRows || seat < 1 || seat > seatsPerRows) {
+    private void validateSeatPosition(int row, int seatNumber) throws InvalidSeatException {
+        if (row < 1 || row > totalRows || seatNumber < 1 || seatNumber > seatsPerRows) {
             throw new InvalidSeatException();
         }
     }
 
-    public void reserveSeat(int row, int numberSeat, String namePerson) {
-
+    public void reserveSeat(int row, int seatNumber, String name) throws InvalidSeatException, SeatAlreadyTakenException {
+        validateSeatPosition(row, seatNumber);
+        Seat chosenSeat = new Seat(row, seatNumber, name);
+        if (seatReserved.contains(chosenSeat)) {
+            throw new SeatAlreadyTakenException();
+        }
+        seatReserved.add(chosenSeat);
     }
 }
