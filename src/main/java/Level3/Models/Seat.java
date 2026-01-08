8e86de6075c630b82604553d835f157b21c6d920
cristhianchulca49 <cristhianchulca49@gmail.com>
2026-01-08 20:37:07 +0100

    feat: override equals and hashCode to use contains comparing rows and seatNumber

--- src/main/java/Level3/Models/Seat.java
+++ src/main/java/Level3/Models/Seat.java
@@ -1,4 +1,17 @@
 package Level3.Models;
 
-public record Seat(int row, int numberSeat, String namePerson) {
+import java.util.Objects;
+
+public record Seat(int row, int seatNumber, String name) {
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Seat seat1)) return false;
+        return row == seat1.row && seatNumber == seat1.seatNumber;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(row, seatNumber);
+    }
 }
