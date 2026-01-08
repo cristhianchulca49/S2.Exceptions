edf0e2d3ae9751f6e2da1d8c7667a5e058b3dd86
cristhianchulca49 <cristhianchulca49@gmail.com>
2026-01-08 21:35:46 +0100

    feat: add Input logic and handle exceptions

--- src/main/java/Level3/ConsoleUI/Input.java
+++ src/main/java/Level3/ConsoleUI/Input.java
@@ -5,6 +5,18 @@ import java.util.Scanner;
 public class Input {
     private static final Scanner sc = new Scanner(System.in);
 
+    public static int readInt(String message) {
+        while (true) {
+            try {
+                System.out.println(message);
+                String aux = sc.nextLine().trim();
+                return Integer.parseInt(aux);
+            } catch (NumberFormatException e) {
+                System.out.println("Sorry, You must write only number, not letters, dot or comma");
+            }
+        }
+    }
+
     public static String readString(String message) {
         System.out.println(message);
         return sc.nextLine().trim();
